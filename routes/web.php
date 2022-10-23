dbfbd943a5c110d1d3f354471a6748afb10652c8
Yoniel Ysturiz <ysturizyoniel@gmail.com>
2022-10-22 23:36:52 -0500

    detalles

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\vacantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('index', [App\Http\Controllers\vacantController::class, 'index']);
